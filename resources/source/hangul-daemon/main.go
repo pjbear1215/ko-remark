f0a2f5bdafb27af1e3535e7f09e54f4faef0db11
Jiwoong <pjbear1215@gmail.com>
2026-03-13 18:27:34 +0900

    allkeymap

--- resources/source/hangul-daemon/main.go
+++ resources/source/hangul-daemon/main.go
@@ -792,43 +792,27 @@ func (d *Daemon) sendKeySequence(codes ...uint16) {
 }
 
 func (d *Daemon) sendMappedKeyTap(key mappedKey) {
-	needShiftDown := key.shifted && !d.shifted
-	needShiftUp := !key.shifted && d.shifted
-	if needShiftUp {
-		_ = d.writeEvent(EV_KEY, KEY_LEFTSHIFT, keyRelease)
-	}
-	if needShiftDown {
+	if key.shifted {
 		_ = d.writeEvent(EV_KEY, KEY_LEFTSHIFT, keyPress)
 	}
 	_ = d.writeEvent(EV_KEY, key.code, keyPress)
 	_ = d.writeEvent(EV_KEY, key.code, keyRelease)
-	if needShiftDown {
+	if key.shifted {
 		_ = d.writeEvent(EV_KEY, KEY_LEFTSHIFT, keyRelease)
-	}
-	if needShiftUp {
-		_ = d.writeEvent(EV_KEY, KEY_LEFTSHIFT, keyPress)
 	}
 	_ = d.writeEvent(EV_SYN, SYN_REPORT, 0)
 }
 
 func (d *Daemon) sendMappedReplace(key mappedKey) {
-	needShiftDown := key.shifted && !d.shifted
-	needShiftUp := !key.shifted && d.shifted
-	if needShiftUp {
-		_ = d.writeEvent(EV_KEY, KEY_LEFTSHIFT, keyRelease)
-	}
 	_ = d.writeEvent(EV_KEY, KEY_BACKSPACE, keyPress)
 	_ = d.writeEvent(EV_KEY, KEY_BACKSPACE, keyRelease)
-	if needShiftDown {
+	if key.shifted {
 		_ = d.writeEvent(EV_KEY, KEY_LEFTSHIFT, keyPress)
 	}
 	_ = d.writeEvent(EV_KEY, key.code, keyPress)
 	_ = d.writeEvent(EV_KEY, key.code, keyRelease)
-	if needShiftDown {
+	if key.shifted {
 		_ = d.writeEvent(EV_KEY, KEY_LEFTSHIFT, keyRelease)
-	}
-	if needShiftUp {
-		_ = d.writeEvent(EV_KEY, KEY_LEFTSHIFT, keyPress)
 	}
 	_ = d.writeEvent(EV_SYN, SYN_REPORT, 0)
 }
